108ae996a215ae04a85e87e1bd8d2a984dfe708d
Pavel Mikula <mikpav@email.cz>
2025-05-21 13:53:49 +0200

    Adjusted weather values

--- src/main/java/me/firestone82/solaxautomation/service/meteosource/model/type/WeatherType.java
+++ src/main/java/me/firestone82/solaxautomation/service/meteosource/model/type/WeatherType.java
@@ -11,31 +11,31 @@ public enum WeatherType {
     // SUNNY
     CLEAR(null, 26, 0.0),
     SUNNY(null, 2, 0.0),
-    MOSTLY_CLEAR(null, 27, 1.0),
-    MOSTLY_SUNNY(null, 3, 1.0),
-    PARTLY_CLEAR(null, 28, 1.5),
-    PARTLY_SUNNY(null, 4, 1.5),
+    MOSTLY_CLEAR(null, 27, 0.5),
+    MOSTLY_SUNNY(null, 3, 0.5),
+    PARTLY_CLEAR(null, 28, 1.0),
+    PARTLY_SUNNY(null, 4, 1.0),
 
     // CLOUDY
-    MOSTLY_CLOUDY(null, 5, 3.0),
-    CLOUDY(null, 6, 3.5),
-    OVERCAST(null, 7, 4.0),
-    OVERCAST_WITH_LOW_CLOUDS(null, 8, 4.0),
-    FOG(null, 9, 4.0),
+    MOSTLY_CLOUDY(null, 5, 2.0),
+    CLOUDY(null, 6, 2.5),
+    OVERCAST(null, 7, 3.0),
+    OVERCAST_WITH_LOW_CLOUDS(null, 8, 3.0),
+    FOG(null, 9, 3.0),
 
     // RAIN
     POSSIBLE_RAIN("PSBL_RAIN", 12, 3),
     POSSIBLE_FREEZING_RAIN(null, 24, 3.0),
     LIGHT_RAIN(null, 10, 3.5),
-    RAIN(null, 11, 5.0),
-    FREEZING_RAIN(null, 23, 5.0),
-    RAIN_SHOWER(null, 13, 5.0),
+    RAIN(null, 11, 4.0),
+    FREEZING_RAIN(null, 23, 4.0),
+    RAIN_SHOWER(null, 13, 4.0),
 
     // SNOW
     POSSIBLE_SNOW("PSBL_SNOW", 18, 3),
     LIGHT_SNOW(null, 16, 3.5),
-    SNOW(null, 17, 5.0),
-    SNOW_SHOWER(null, 19, 5.0),
+    SNOW(null, 17, 4.0),
+    SNOW_SHOWER(null, 19, 4.0),
 
     // MIXED
     POSSIBLE_RAIN_AND_SNOW(null, 21, 3.0),
